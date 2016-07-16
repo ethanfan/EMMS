219a51824d78180ffc7bccacd561b8748b7d9779
ethanfan <6500368@qq.com>
2016-07-16 09:29:29 +0800

    增加image-loader

--- app/src/main/java/com/emms/fragment/LinkedOrdersFragment.java
+++ app/src/main/java/com/emms/fragment/LinkedOrdersFragment.java
@@ -24,10 +24,10 @@ import com.emms.util.SharedPreferenceManager;
 import com.flyco.tablayout.SegmentTabLayout;
 import com.flyco.tablayout.listener.OnTabSelectListener;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
-import com.jaffer_datastore_android_sdk.datastore.ObjectElement;
-import com.jaffer_datastore_android_sdk.rest.JsonArrayElement;
-import com.jaffer_datastore_android_sdk.rxvolley.client.HttpCallback;
-import com.jaffer_datastore_android_sdk.rxvolley.client.HttpParams;
+import com.datastore_android_sdk.datastore.ObjectElement;
+import com.datastore_android_sdk.rest.JsonArrayElement;
+import com.datastore_android_sdk.rxvolley.client.HttpCallback;
+import com.datastore_android_sdk.rxvolley.client.HttpParams;
 
 import java.util.ArrayList;
 
