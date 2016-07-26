e0f1c6765a91654b524a3af382f4b537a57ac324
ayiao <376638297@qq.com>
2016-07-26 11:17:29 +0800

    增加任务总结以及评价界面UI

---
+++ app/src/main/java/com/emms/activity/CommandActivity.java
@@ -0,0 +1,14 @@
+package com.emms.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/7/25.
+ */
+public class CommandActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView();
+    }
+}
---
+++ app/src/main/java/com/emms/activity/SummuryActivity.java
@@ -0,0 +1,16 @@
+package com.emms.activity;
+
+import android.os.Bundle;
+
+import com.emms.R;
+
+/**
+ * Created by Administrator on 2016/7/25.
+ */
+public class SummuryActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summury);
+    }
+}
